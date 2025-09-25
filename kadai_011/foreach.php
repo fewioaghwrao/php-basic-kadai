d57b5913dda4321abb6df3f7f36ab8dcff8394d5
fewioaghwrao <nrk13959@gmail.com>
2025-09-26 06:57:54 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>課題11</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       $yasai_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' =>  '北海道'];
+
+        //  連想配列$yasai_dataのキーと値を1つずつ順番に出力する
+          foreach ($yasai_data as $key => $value) {
+              echo "{$key}：{$value}<br>";
+          }
+          
+       ?>
+   </p>
+</body>
+
+</html>
